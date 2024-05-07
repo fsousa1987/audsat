aa3de8e9059973f08d2edb40c6300fae22650ab2
Francisco Sousa <franciscoeds1987@gmail.com>
2024-05-07 12:38:12 -0300

    feat/added start to init application

---
+++ src/main/java/br/com/francisco/test/audsat/AudsatApplication.java
@@ -0,0 +1,13 @@
+package br.com.francisco.test.audsat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudsatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudsatApplication.class, args);
+    }
+
+}
